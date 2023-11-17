ac22b6bfea7e3aaa2c5ceec151e9cbac06c287ab
Derki26 <Derki26.github.com>
2023-11-16 23:28:45 -0500

    todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,22 @@ function App() {
     setTodo(newTodoList)
   }
 
+  const changeTodoState = (id, state) => {
+    const newTodoList = todos.map((item) => {
+      if (item.id === id) {
+        return { ...item, done: state };
+      } else {
+        return item;
+      }
+    });
+
+    setTodo(newTodoList);
+  };
+
   return (
     <div className="container">
       <h1>
-        {"<"}My To do list{"/>"}
+      {"<"} My To do list {">"}
       </h1>
 
       <div className="new-todo-container">
@@ -59,14 +71,19 @@ function App() {
       <ul className="todo-list">
         {todos.map((item) => {
           return (
-            <li key={item.id} className="todo-item">
-              <input type="checkbox" value={item.done} />
-              <span className="todo-item-text">{item.task}</span>
-              <button 
-              onClick={()=> deleteTodo(item.id)
-              } className="delete"
-              >❌</button>
-            </li>
+            <li key={item.id} className={`todo-item ${item.done?"done" : ""}`}>
+            <input
+              type="checkbox"
+              value={item.done}
+              onChange={(e) => {
+                changeTodoState(item.id, e.target.checked);
+              }}
+            />
+            <span className="todo-item-text">{item.task}</span>
+            <button onClick={() => delteTodo(item.id)} className="delete">
+              ❌
+            </button>
+          </li>
           );
         })}
       </ul>
